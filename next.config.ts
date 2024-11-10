1ac69be68d547b33967e99680714338d1b5f4618
swapnilchougule00 <swapnilchougule00@gmail.com>
2024-11-11 01:39:16 +0530

    fix: toast

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // Ignores TypeScript errors during the build
-  },
   /* Add other config options here */
 };
 
